a9f47e2bdf0629e8fd45e4408256e8248b798784
appdev0111 <70638220+appdev0111@users.noreply.github.com>
2020-09-22 10:55:38 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// HTML INJECTION
+ReactDOM.render(
+  <div>
+    <h1>Hello Word</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
